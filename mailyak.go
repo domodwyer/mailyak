cfafc8877e286be72e0e7f635e11c60ea2869326
Daniel Lohse <info@asapdesign.de>
2016-08-12 15:32:46 +0200

    Make MailYak struct conform to the Stringer interface
    I also added a test and made sure that no auth credentials are leaked
    when this is used to log a mail object via a logger.

--- mailyak.go
+++ mailyak.go
@@ -1,6 +1,7 @@
 package mailyak
 
 import (
+	"fmt"
 	"net/smtp"
 	"regexp"
 )
@@ -76,6 +77,17 @@ func (m MailYak) Send() error {
 	}
 
 	return nil
+}
+
+// String makes MailYak struct printable for debugging purposes (conforms to the Stringer interface).
+func (m *MailYak) String() string {
+	var att []string
+	for _, a := range m.attachments {
+		att = append(att, "{filename: "+a.filename+"}")
+	}
+	return fmt.Sprintf("&MailYak{from: %q, fromName: %q, html: %v bytes, plain: %v bytes, toAddrs: %v, bccAddrs: %v, subject: %q, host: %q, attachments (%v): %v, auth set: %v}",
+		m.fromAddr, m.fromName, len(m.HTML().String()), len(m.Plain().String()), m.toAddrs, m.bccAddrs, m.subject, m.host, len(att), att, m.auth != nil,
+	)
 }
 
 // HTML returns a BodyPart for the HTML email body
