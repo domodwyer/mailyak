8b51c9b282d3a4ada40b1bc2063c715339f7f619
Dom <dom@itsallbroken.com>
2016-08-17 17:30:35 +0100

    Fix typo

--- mailyak_test.go
+++ mailyak_test.go
@@ -12,7 +12,7 @@ func TestMailYakStringer(t *testing.T) {
 	mail := New("mail.host.com:25", smtp.PlainAuth("", "user", "pass", "mail.host.com"))
 	mail.From("from@example.org")
 	mail.FromName("From Example")
-	mail.To("to@exmaple.org")
+	mail.To("to@example.org")
 	mail.Bcc("bcc1@example.org", "bcc2@example.org")
 	mail.Subject("Test subject")
 	mail.ReplyTo("replies@example.org")
@@ -21,7 +21,7 @@ func TestMailYakStringer(t *testing.T) {
 	mail.Attach("test.html", strings.NewReader("<html><head></head></html>"))
 	mail.Attach("test2.html", strings.NewReader("<html><head></head></html>"))
 
-	want := "&MailYak{from: \"from@example.org\", fromName: \"From Example\", html: 31 bytes, plain: 42 bytes, toAddrs: [to@exmaple.org], bccAddrs: [bcc1@example.org bcc2@example.org], subject: \"Test subject\", host: \"mail.host.com:25\", attachments (2): [{filename: test.html} {filename: test2.html}], auth set: true}"
+	want := "&MailYak{from: \"from@example.org\", fromName: \"From Example\", html: 31 bytes, plain: 42 bytes, toAddrs: [to@example.org], bccAddrs: [bcc1@example.org bcc2@example.org], subject: \"Test subject\", host: \"mail.host.com:25\", attachments (2): [{filename: test.html} {filename: test2.html}], auth set: true}"
 	got := fmt.Sprintf("%+v", mail)
 	if got != want {
 		t.Errorf("MailYak.String() = %v, want %v", got, want)
